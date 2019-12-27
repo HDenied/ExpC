b91b2a2e83c4f05262fc8836651fb7bbef33b405
Simone Greco <simone.greco@limejump.com>
2019-12-27 10:40:23 +0000

    Added argument parsing, updated readme file

--- wordCount/src/README.md
+++ wordCount/src/README.md
@@ -1,27 +1,37 @@
 ## Build && run program
+```
 cd src
-gcc -g -o sorter  ./utils/utils.c ./tst_tree/tst.c ./heap_tree/heap.c ./main.c -I. -Itst_tree -Iutils -Iheap_tree
-./sorter
+gcc -g -o ../wcounter  ./utils/utils.c ./tst_tree/tst.c ./heap_tree/heap.c ./main.c -I. -Itst_tree -Iutils -Iheap_tree
+../wcounter file_path [-n words_number]
+```
 
 ## Build && run heap tests
+```
 cd heap_tree
 gcc -g -o unit_test unit_test.c heap.c ../utils/utils.c -I. -I../utils/
 ./unit_test
+```
 
 ## Build && run tst tests
+```
 cd tst_tree
 gcc -g -o unit_test unit_test.c tst.c ../utils/utils.c -I. -I../utils/
 ./unit_test
+```
 
 ## Build && run utils tests
+```
 cd utils
 gcc -g -o unit_test unit_test.c utils.c -I.
 ./unit_test
+```
 
 ## GCC VERSION
-gcc (GCC) 9.2.0 (ArchLinux)
-gcc 6.3.0 (Debian)
+
+- gcc (GCC) 9.2.0 (ArchLinux)
+- gcc 6.3.0 (Debian)
 
 ## Machines:
-Linux XXXX 5.4.2-arch1-1 #1 SMP PREEMPT Thu, 05 Dec 2019 12:29:40 +0000 x86_64 GNU/Linux
-Linux debian9 4.9.0-8-amd64 #1 SMP Debian 4.9.144-3.1 (2019-02-19) x86_64 GNU/Linux
+
+- Linux XXXX 5.4.2-arch1-1 #1 SMP PREEMPT Thu, 05 Dec 2019 12:29:40 +0000 x86_64 GNU/Linux
+- Linux debian9 4.9.0-8-amd64 #1 SMP Debian 4.9.144-3.1 (2019-02-19) x86_64 GNU/Linux
--- wordCount/src/main.c
+++ wordCount/src/main.c
@@ -2,18 +2,93 @@
 #include<tst.h>
 #include<heap.h>
 
-int main()
+struct _config_s 
+{
+    char *f_path;
+    int num_words;
+};
+
+typedef struct _config_s CONFIG_S;
+
+void parse_config(CONFIG_S *conf, uint num_args, char *args[])
+{
+    FILE *f=NULL;
+    uint idx=0;
+    
+    conf->num_words=-1;
+
+    if(num_args==1)
+    {
+        printf("At least an argument should be provided, type '-h' for help\n");
+        exit(1);
+    }
+
+    for(idx=1; idx<num_args; idx++)
+    {
+        if(!strcmp(args[idx],"-h"))
+        {
+            printf("\nNAME:\n\twcounter - counts the top N words occurrances in a file\n");
+            printf("\nSYNOPSIS:\n\twcounter file_full_path [-n words_number]\n\n");
+            exit(0);
+        }
+    }
+
+    conf->f_path = args[1];
+    if(strchr(conf->f_path, '\\') || strchr(conf->f_path,'-'))
+    {
+        log_err("Not valid linux path: '%s'",conf->f_path);
+        exit(1);
+    }
+
+    f=fopen(conf->f_path,"rb");
+    if(f)
+        fclose(f);
+    else
+    {
+        log_err("File '%s' doesn't exist", conf->f_path);
+        exit(1);
+    }
+
+    idx=2;
+    while(idx<num_args)
+    {
+        if(!strcmp(args[idx],"-n"))
+        {
+            if (idx+1<num_args)
+            {
+                sscanf(args[idx+1], "%d", &(conf->num_words));
+            }
+            else
+            {
+                log_err("Missing number of top words to display after '-n'");
+                exit(1);
+            }
+            
+        }
+
+        idx++;
+
+    }
+
+
+}
+
+int main(int argc, char *argv[])
 {
 
     FILE *fp;
+    CONFIG_S conf;
     TST_TREE *tree=NULL;
     HEAP_TREE *h_tree = NULL;
     WORD_S word_s;
-    char f_path[]="../mobydick.txt";
     char word[W_LEN]="\0";    
-    fp = fopen(f_path, "rb");
     DATA tmp_data;
     uint longest_w_size=0;
+
+
+    parse_config(&conf, argc, argv);
+
+    fp = fopen(conf.f_path, "rb");
 
     /* Populating TST tree */
     while(UTILS_tokenize(fp, word))
@@ -38,7 +113,11 @@ int main()
     }
     
     HEAP_sort(h_tree, HEAP_cmp_occ);
-    HEAP_print_tree(h_tree,20);
+    if(conf.num_words>=0)
+        HEAP_print_tree(h_tree, conf.num_words);
+    else
+        HEAP_print_tree(h_tree, HEAP_get_num_w(h_tree));
+    
     HEAP_erase(&h_tree);
 
     return 0;
